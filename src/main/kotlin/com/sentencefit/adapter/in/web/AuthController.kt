c1e0b691549f743f1d71b119c4c2058c34a206e1
hoshino-love0102 <hoshino.whale0102@gmail.com>
2025-12-09 19:55:21 +0900

    refactor: remove api prefix from auth and user routes

--- src/main/kotlin/com/sentencefit/adapter/in/web/AuthController.kt
+++ src/main/kotlin/com/sentencefit/adapter/in/web/AuthController.kt
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 class AuthController(
     private val sendEmailVerificationUseCase: SendEmailVerificationUseCase,
     private val verifyEmailUseCase: VerifyEmailUseCase,
